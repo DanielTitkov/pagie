9d590c062e3036e60db2895608852033e3c1ae61
Daniil <mytasmic@gmail.com>
2019-05-27 22:42:59 +0300

    ready for running alpha?

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-domains=(pagieapp.tech  www.pagieapp.tech)
+domains=(pagieapp.com  www.pagieapp.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mytasmic@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 container_name=frontend # service name in docker-compose file
-compose_files="-f docker-compose.yml -f docker-compose.stage.yml"
+compose_files="-f docker-compose.yml -f docker-compose.prod.yml"
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
